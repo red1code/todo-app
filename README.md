d7cddf277b6569d214866ec893a115e417230dc5
Redouane Bekkouche <redouane.bekk@gmail.com>
2023-07-11 18:28:39 +0100

    setup environement

---
+++ README.md
@@ -0,0 +1,4 @@
+# Description
+A todo app project following [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-todo-list) curriculum
+
+Live preview [here](#)
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './docs'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: 'src/template.html',
+      // favicon: 'src/assets/library-icon.png'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+    clean: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  }
+}
