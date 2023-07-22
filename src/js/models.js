0601720290f20d2804cb0e0730275101d981fde8
Redouane Bekkouche <redouane.bekk@gmail.com>
2023-07-22 21:08:35 +0100

    replace classes with factory functions in models.js

--- src/js/models.js
+++ src/js/models.js
@@ -1,28 +1,49 @@
 export {
   Todo,
-  Project
+  List
 }
 
 
-class Todo {
-  constructor(id, title, description, dueDate, priority, createdAt, isCompleted, parentProject) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.createdAt = createdAt;
-    this.isCompleted = isCompleted;
-    this.parentProject = parentProject;
+function Todo(id, title, description, dueDate, createdAt, isCompleted, parentProject) {
+  return {
+    id,
+    title,
+    description,
+    dueDate,
+    createdAt,
+    isCompleted,
+    parentProject
+  }
+}
+
+function List(id, title, createdAt) {
+  return {
+    id,
+    title,
+    createdAt
   }
 }
 
 
-class Project {
-  constructor(id, title, priority, createdAt) {
-    this.id = id;
-    this.title = title;
-    this.priority = priority;
-    this.createdAt = createdAt;
-  }
-}
+// class Todo {
+//   constructor(id, title, description, dueDate, priority, createdAt, isCompleted, parentProject) {
+//     this.id = id;
+//     this.title = title;
+//     this.description = description;
+//     this.dueDate = dueDate;
+//     this.priority = priority;
+//     this.createdAt = createdAt;
+//     this.isCompleted = isCompleted;
+//     this.parentProject = parentProject;
+//   }
+// }
+
+
+// class Project {
+//   constructor(id, title, priority, createdAt) {
+//     this.id = id;
+//     this.title = title;
+//     this.priority = priority;
+//     this.createdAt = createdAt;
+//   }
+// }
